ce026b2ec2473753f47e830930f349581c483c83
yogeswar <yogi2198@gmail.com>
2020-09-19 07:56:02 +0530

    Filterable list with JS

---
+++ FilterableList/script.js
@@ -0,0 +1,18 @@
+let filterInput = document.getElementById('filterInput');
+
+filterInput.addEventListener('keyup', filterNames);
+
+function filterNames(){
+  let filterValue = document.getElementById('filterInput').value.toUpperCase();
+  let ul = document.getElementById('names');
+  let li = ul.querySelectorAll('li.collection-item')
+  for (let i = 0; i < li.length; i++) {
+    let a=li[i].getElementsByTagName('a')[0];
+    if(a.innerHTML.toUpperCase().indexOf(filterValue) > -1){
+      li[i].style.display='';
+    }else{
+      li[i].style.display = 'none'
+    }
+    
+  }
+}
