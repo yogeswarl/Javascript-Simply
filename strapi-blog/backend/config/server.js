522e1270019e515e201f40986315603362600d24
yogeswar <yogi2198@gmail.com>
2021-05-16 15:25:10 +0530

    strapi config frontend and backend for blog

---
+++ strapi-blog/backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '27171449314a38e02978272a0099bac3'),
+    },
+  },
+});
---
+++ strapi-blog/backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8f37a017-3a34-48ad-8cc2-e9a205ed771b'
+};
