c3ffc5d83d08cacffd14d92c43fa14da5d53125d
yogeswar <yogi2198@gmail.com>
2021-05-22 12:17:43 +0530

    handlebars setup. initial route declaration

--- storybooks-node/app.js
+++ storybooks-node/app.js
@@ -1,9 +1,24 @@
+const path = require('path')
 const express = require("express");
 const dotenv = require("dotenv");
-const connectDB = require('./config/db')
+const morgon = require('morgan');
+const hbs = require('express-handlebars')
+// internal files
+const connectDB = require('./config/db');
+
 dotenv.config({ path: "./config/config.env" });
 connectDB()
 const app = express();
+
+if(process.env.NODE_ENV === 'development') {
+	app.use(morgon('dev'))
+}
+
+
+app.engine("hbs", hbs({ defaultLayout: "main" , extname: "hbs" }));
+app.set('view engine', 'hbs')
+app.use(express.static(path.join(__dirname,'public')))
+app.use('/',require('./routes/index'))
 
 const PORT = process.env.PORT || 5000;
 app.listen(
---
+++ storybooks-node/routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router();
+
+router.get('/',(req,res) =>{
+  res.render('login',{
+    layout:'login'
+  });
+})
+router.get('/dashboard',(req,res) =>{
+  res.render('dashboard');
+})
+
+module.exports = router
