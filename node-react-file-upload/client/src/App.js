f2554af10c4bf3c33c3e0928272b3b433c3063c4
yogeswar <yogi2198@gmail.com>
2021-05-31 08:03:31 +0530

    datepicker try

--- node-react-file-upload/client/src/App.js
+++ node-react-file-upload/client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import FileUpload from './components/FileUpload';
+import DatePicker from './components/DatePicker'
 
 const App = () => (
 <div className='container mt-4'>
     <h4 className="display-4 text-center mb-4">React File Upload</h4>
     <FileUpload />
+    <DatePicker />
 </div>
 );
 
---
+++ node-react-file-upload/client/src/components/DatePicker.jsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react"
+
+const DatePicker = () =>{
+  return(
+    <Fragment>
+      <input type="date" name="date" id="date" />
+    </Fragment>
+    )
+}
+
+export default DatePicker
